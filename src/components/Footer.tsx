b2c6801200e76d9c46febe02a05a8acd12a5ec71
Robin Singh <robinsingh1912@gmail.com>
2023-08-15 17:10:27 +0530

    style: :lipstick: updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,12 +1,19 @@
-import { BsLinkedin } from 'react-icons/bs';
-
 export function Footer() {
   return (
-    <footer className="py-4 border-t border-gray-800">
-      <div className="container flex justify-between">
-        <p className="text-xs text-gray-300">
-          Inspired by MongoDB Starter and coded with ❤️ in 🇮🇳. Built with
-          Next.js and Tailwind CSS, deployed with Vercel.
+    <footer className='py-4 border-t border-gray-800 bg-gray-950'>
+      <div className='container flex justify-between'>
+        <p className='text-xs text-gray-300'>
+          Inspired by{' '}
+          <a
+            href='https://mongodb.vercel.app/'
+            target='_blank'
+            rel='noreferrer'
+            className='underline underline-offset-2'
+          >
+            MongoDB Starter
+          </a>{' '}
+          and coded with ❤️ in 🇮🇳. Built with Next.js and Tailwind CSS, deployed
+          with Vercel.
         </p>
       </div>
     </footer>
