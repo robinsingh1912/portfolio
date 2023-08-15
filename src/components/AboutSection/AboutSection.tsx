93017d2440784b0d520865f8e2b4ff3ed6998437
Robin Singh <robinsingh1912@gmail.com>
2023-08-15 14:32:25 +0530

    fix: :ambulance: build was broken bcz of eslint and husky

--- src/components/AboutSection/AboutSection.tsx
+++ src/components/AboutSection/AboutSection.tsx
@@ -2,14 +2,14 @@ import Data from './data.json';
 
 export default function AboutSection() {
   return (
-    <section id="about" className={`container mt-16`}>
-      <h2 className="font-semibold font-mono text-2xl">About</h2>
-      <article className="max-w-2xl mt-3 gap-4 grid">
+    <section id='about' className={`container mt-16`}>
+      <h2 className='font-semibold font-mono text-2xl'>About</h2>
+      <article className='max-w-2xl mt-3 gap-4 grid'>
         <p>
-          Hi there! <span className="text-2xl">👋</span>, I’m{' '}
-          <span className="bg-pink-600 p-1">Robin Singh</span>! from New Delhi,
+          Hi there! <span className='text-2xl'>👋</span>, I’m{' '}
+          <span className='bg-pink-600 p-1'>Robin Singh</span>! from New Delhi,
           India, a passionate self-taught{' '}
-          <span className="underline underline-offset-2">
+          <span className='underline underline-offset-2'>
             Full-Stack Engineer
           </span>{' '}
           with over 5 years of experience. I take pride in writing clean,
@@ -18,14 +18,14 @@ export default function AboutSection() {
           code quality.
         </p>
         <p>Here are a few technologies I’ve been working with recently:</p>
-        <div className="flex flex-wrap gap-10">
+        <div className='flex flex-wrap gap-10'>
           {Data.map((technologies) => (
-            <ul>
-              <p className="underline underline-offset-4">
+            <ul key={technologies.title}>
+              <p className='underline underline-offset-4'>
                 {technologies.title}
               </p>
               {technologies.technologies.map((technology) => (
-                <li>{technology}</li>
+                <li key={technology}>{technology}</li>
               ))}
             </ul>
           ))}
