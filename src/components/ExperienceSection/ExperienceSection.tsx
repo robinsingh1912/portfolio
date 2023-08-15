1afa71eb5631157b1ee7a959050d73a238b6b137
Robin Singh <robinsingh1912@gmail.com>
2023-08-15 14:20:36 +0530

    feat(experience): :sparkles: "See All" button added

--- src/components/ExperienceSection/ExperienceSection.tsx
+++ src/components/ExperienceSection/ExperienceSection.tsx
@@ -1,54 +1,47 @@
-import { BsArrowRight } from 'react-icons/bs';
+'use client';
+import { BsArrowRight, BsChevronRight } from 'react-icons/bs';
 import EXPERIENCES from './experiences.json';
+import { useState } from 'react';
+import classNames from '@/utils/classNames';
+import { ExperienceListItem } from './ExperienceListItem';
 
-const DEFAULT_LOGO = 'https://static.licdn.com/sc/h/aajlclc14rr2scznz5qm2rj9u';
+const MAX_EXPERIENCE = 4;
+const TOP_EXPERIENCES = EXPERIENCES.slice(0, MAX_EXPERIENCE);
+const OTHER_EXPERIENCES = EXPERIENCES.slice(MAX_EXPERIENCE);
+
 export default function ExperienceSection() {
+  const [collapsed, setCollapsed] = useState(true);
   return (
     <section id='experience' className='container mt-16'>
       <h2 className='font-semibold font-mono text-2xl mb-4'>Experience</h2>
-      <div className='divide-y divide-gray-800'>
-        {EXPERIENCES.map((experience) => (
-          <div className='flex gap-4 py-4' key={experience.companyName}>
-            <a href={experience.companyUrl} target='_blank'>
-              <img
-                src={experience.companyLogo || DEFAULT_LOGO}
-                alt={`${experience.companyName} logo`}
-                className='w-12 h-12 object-cover object-center'
-              />
-            </a>
-            <div className='flex flex-col flex-1'>
-              <h3>{experience.title}</h3>
-              <small>
-                {experience.companyName} · {experience.employmentType}
-              </small>
-              <small>
-                {experience.startDate} - {experience.endDate || 'Present'}
-              </small>
-              <small>
-                {experience.location} · {experience.locationType}
-              </small>
-              {experience.description && (
-                <p className='text-xs [text-wrap:balance] mt-2'>
-                  {experience.description}
-                </p>
-              )}
-              <div className='flex gap-2 mt-2 flex-wrap'>
-                {experience.skills?.map((skill) => (
-                  <div
-                    className='inline-flex justify-center px-2 py-1 border border-gray-800 hover:border-white shadow-sm font-medium rounded-md text-white text-xs font-mono bg-black focus:outline-none focus:ring-0 transition-all'
-                    key={skill}
-                  >
-                    {skill}
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-        ))}
-      </div>
+      <ul className='divide-y divide-gray-800'>
+        {[...TOP_EXPERIENCES, ...(collapsed ? [] : OTHER_EXPERIENCES)].map(
+          (experience) => (
+            <ExperienceListItem
+              experience={experience}
+              key={experience.companyName}
+            />
+          )
+        )}
+      </ul>
+      <button
+        className='w-full bg-gray-900 group hover:bg-gray-800 py-2 px-4 border-gray-800 hover:border-gray-300 transition-all border font-mono text-sm rounded-md inline-flex items-center justify-center gap-2'
+        onClick={() => setCollapsed(!collapsed)}
+      >
+        {collapsed
+          ? `Show all ${EXPERIENCES.length} `
+          : `Show top ${MAX_EXPERIENCE} `}
+        experiences
+        <BsChevronRight
+          className={classNames('transition-all', {
+            'group-hover:rotate-90': collapsed,
+            'group-hover:-rotate-90': !collapsed,
+          })}
+        />
+      </button>
       <a
         href='./resume.pdf'
-        className='font-mono font-semibold text-sm inline-flex items-center gap-1 hover:gap-3 hover:underline underline-offset-4 transition-all'
+        className='font-mono font-semibold text-sm inline-flex items-center gap-1 hover:gap-3 hover:underline underline-offset-4 transition-all mt-4'
       >
         View Full Resume <BsArrowRight />
       </a>
